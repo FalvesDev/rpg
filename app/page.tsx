d22b70d20a6ad308ffc64e32ba9d3bb023c8ccbc
Felipe Alves <fdevalves@gmail.com>
2023-03-11 18:48:36 -0300

    Coisas sendo feitas

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import Hero from '@/src/sections/Hero'
+import Personagens from '@/src/sections/Personagens'
 import React from 'react'
 
 export default function Home() {
   return (
     <>
       <Hero/>
+      <Personagens/>
     </>
   )
 }
---
+++ src/sections/Personagens.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+type Props = {}
+
+export default function Personagens({}: Props) {
+  return (
+    <div className="flex justify-center text-2xl uppercase bg-white ">Tem coisas sendo construídas, aguarde...</div>
+  )
+}
